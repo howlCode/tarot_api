1b824b60331590065701cffd4661ccc95074862d
micromanyth <miromanyth@gmail.com>
2018-02-23 15:45:52 -0500

    Wrote scraper to pull in card info for seeds.db. It's in lib folder

---
+++ lib/tarot_scraper.rb
@@ -0,0 +1,17 @@
+
+require 'nokogiri'
+require 'open-uri'
+
+# Pry.start(binding)
+card_name_major = ['the-fool', 'the-magician', 'the-empress', 'the-emperor', 'the-high-priestess',
+             'the-hierophant', 'the-lovers', 'the-chariot', 'justice', 'temperance',
+             'strength', 'the-hermit', 'the-wheel-of-fortune', 'the-hanged-man',
+             'death', 'the-devil', 'the-tower', 'the-star', 'the-moon', 'the-sun',
+             'judgement', 'the-world']
+
+card_name_major.each do |c|
+  page_for_card_info = "https://www.simplytarot.com/tarot-card-meanings/major-arcana-tarot-card-meanings/#{c}/"
+  page = Nokogiri::HTML(open(page_for_card_info))
+  page_element = page.css("div.card p")
+  puts page_element.text
+end
