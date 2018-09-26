ca756e880ec203c94424facf3da52520686633c2
howlCode <miromanyth@gmail.com>
2018-09-26 14:33:35 -0400

    Set local ruby version to 2.4.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.10.1"
 
 set :application, "tarot_api"
 set :repo_url, "https://github.com/howlCode/tarot_api.git"
