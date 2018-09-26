6daf0c01844265ea4f1d7953c0bd42be3ffb4c17
howlCode <miromanyth@gmail.com>
2018-09-26 14:09:30 -0400

    Update README for new versioning and naming for endpoints

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "~> 3.11.0"
 
 set :application, "tarot_api"
 set :repo_url, "https://github.com/howlCode/tarot_api.git"
